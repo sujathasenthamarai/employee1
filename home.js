75f29ee0162ec39db3bb14e02cdcdea1ae47225e
sujathasenthamarai <161680625+sujathasenthamarai@users.noreply.github.com>
2024-08-18 23:12:54 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,203 @@
+module.exports=()=>{
+    return `
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styel.css">
+    <title>Document</title>
+</head>
+<body>
+
+<form id="enterance">
+    
+    <button type="button" onclick="sendval(1)">ADD DATA</button>
+    <button type="button" onclick="sendval(2)">DELETE DATA</button>
+    <button type="button" onclick="sendval(3)">UPDATE DATA</button>
+</form>
+
+
+    <form  action="/" method="POST">
+        
+        <div id="addform">
+        <input type="text" id="hidden" name="hidden"  placeholder=".">
+        <table class="formtab">
+            <tr>
+       <td> <label>Employee Id :</label> </td><td><input type="text" name="id" id="id" placeholder="Id"></td> <td><input type="text" name="idu" id="idu" placeholder="Id"><br></td>
+            </tr>
+            <tr>
+                <td> <label>Employee Name:</label></td><td><input type="text" name="name" id="name" placeholder="Name"></td><td><input type="text" name="nameu" id="nameu" placeholder="Name"><br></td>
+           </tr>
+           <tr>
+            <td> <label>Employee Age:</label></td><td><input type="number" name="age" id="age" placeholder="Age"></td><td><input type="number" name="ageu" id="ageu" placeholder="Age"><br></td>
+    </tr>
+    <tr>
+        <td>   <label>Employee Designation:</label></td><td><input type="text" name="desg" id="desg" placeholder="Designation"></td><td><input type="text" name="desgu" id="desgu" placeholder="Designation"><br></td>
+    </tr>
+    <tr>
+        <td>   <label>Employee MObile No:</label></td><td><input type="number" name="mobile" id="mobile" placeholder="Mobile no."></td><td><input type="number" name="mobileu" id="mobileu" placeholder="Mobile no."><br></td>
+    </tr>
+    <tr>
+       <td> <button type="submit" id="add" onclick="backto()">ADD Employee</button></td>
+    </tr>
+
+    </table>
+    </div>
+        <div id="deleteform">
+        
+            <label>Employee Id :</label><input type="text" name="did" placeholder="Delete Using Employee Id"><br>
+            
+            <button type="submit" onclick="backto()">Delete Employee</button>
+            
+        </div>
+        
+    </form>
+
+   
+        <div id="updateform">
+            <label>Employee Id :</label>
+            <input type="text" name="uid" id="uid" placeholder="update Using Employee Id"><br>
+            <button type="submit" onclick="getdata()">Get emp datails</button>
+        </div>
+    
+
+   
+
+<br>
+<div>
+    <table cellpadding="3" cellspacing="2" border="1" bgcolor="#dfdfdf" id="ttt">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Designation</th>
+                <th>Mobile</th>
+            </tr>
+        </thead>
+            <tbody id="tableData"></tbody>
+        </table>
+        
+</div>
+    <script>
+         document.getElementById('hidden').style.display='none';
+        document.getElementById('addform').style.display='none';
+        document.getElementById('deleteform').style.display='none';
+        document.getElementById('updateform').style.display='none';
+        document.getElementById('idu').style.display='none';
+        document.getElementById('nameu').style.display='none';
+        document.getElementById('ageu').style.display='none';
+        document.getElementById('desgu').style.display='none';
+        document.getElementById('mobileu').style.display='none';
+        var objfile=[];
+
+        fetch('/data').then(response => response.json()).then(user => {
+           const datas=user;
+           objectLength = Object.keys(datas).length;
+           
+
+           var k = '<tbody>'
+            for(i = 0;i < objectLength; i++){
+                objfile[i]=datas[i];
+                k+= '<tr id="tabr">';
+                k+= '<td>' + datas[i].id + '</td>';
+                k+= '<td>' + datas[i].name + '</td>';
+                k+= '<td>' + datas[i].age + '</td>';
+                k+= '<td>' + datas[i].designation + '</td>';
+                k+= '<td>' + datas[i].mobile + '</td>';
+                k+= '</tr>';
+            }
+            k+='</tbody>';
+            document.getElementById('tableData').innerHTML = k;
+           
+        });
+
+
+
+
+       
+
+
+
+    function getdata(){
+        document.getElementById('addform').style.display='block';
+        document.getElementById('idu').style.display='inline-block';
+        document.getElementById('nameu').style.display='inline-block';
+        document.getElementById('ageu').style.display='inline-block';
+        document.getElementById('desgu').style.display='inline-block';
+        document.getElementById('mobileu').style.display='inline-block';
+        document.getElementById('enterance').style.display='none';
+        //document.getElementById('ttt').style.display='none';
+        document.getElementById('updateform').style.display='none';
+        console.log(objectLength);
+        var stval=document.getElementById('uid').value;
+        console.log(stval);
+        for(i = 0;i < objectLength; i++){
+            if(stval === objfile[i].id){
+                document.getElementById('idu').value=objfile[i].id;
+                document.getElementById('nameu').value=objfile[i].name;
+                document.getElementById('ageu').value=objfile[i].age;
+                document.getElementById('desgu').value=objfile[i].designation;
+                document.getElementById('mobileu').value=objfile[i].mobile;
+
+                document.getElementById('id').value=objfile[i].id;
+                document.getElementById('name').value=objfile[i].name;
+                document.getElementById('age').value=objfile[i].age;
+                document.getElementById('desg').value=objfile[i].designation;
+                document.getElementById('mobile').value=objfile[i].mobile;
+
+                //disable the values
+                document.getElementById("id").disabled = true;
+                document.getElementById("idu").disabled = true;
+                document.getElementById("name").disabled = true;
+                document.getElementById("age").disabled = true;
+                document.getElementById("desg").disabled = true;
+                document.getElementById("mobile").disabled = true;
+                
+                 document.getElementById('hidden').value=3;
+                 document.getElementById('add').innerHTML="Update";
+           console.log(document.getElementById('hidden').value);
+            }
+        }
+    
+    }
+        
+
+
+        function sendval(n){
+            document.getElementById('hidden').value=n;
+            if(n==1){
+                document.getElementById('addform').style.display='block';
+                document.getElementById('enterance').style.display='none';
+            }else if(n==2){
+                document.getElementById('deleteform').style.display='block';
+                document.getElementById('enterance').style.display='none';
+            }else if(n==3){
+                document.getElementById('updateform').style.display='block';
+                document.getElementById('enterance').style.display='none';
+            }
+            console.log(document.getElementById('hidden').value);
+        }
+
+        function backto(){
+//enamble the text box
+            document.getElementById("id").disabled = false;
+            ocument.getElementById("idu").disabled = false;
+                document.getElementById("name").disabled = false;
+                document.getElementById("age").disabled = false;
+                document.getElementById("desg").disabled = false;
+                document.getElementById("mobile").disabled = false;
+
+            document.getElementById('addform').style.display='none';
+            document.getElementById('deleteform').style.display='none';
+                document.getElementById('enterance').style.display='block';
+        }
+    </script>
+</body>
+</html>
+`
+}
---
+++ index.js
@@ -0,0 +1,73 @@
+const express=require("express");
+const app=express();
+const mongoose=require('mongoose');
+const port=3000;
+const bodyparser=require("body-parser");
+const path=require("path");
+const htmlfile=require("./home")
+
+
+app.use(express.static(path.join(__dirname,'..','employee-data')));
+
+app.use(bodyparser.json());
+
+var urlencodedparser=bodyparser.urlencoded({extended: true})
+
+const url=`mongodb+srv://sujathasenthamarai:suji$17yazh@cluster0.oquejho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.connect(url)
+    .then((x)=>{console.log(`Connected Successfully! ${x.connections[0].name} `)})
+    .catch((err)=>{console.error(`connection Problem: ${err}`)});
+
+const empdata=mongoose.Schema(
+    {
+        id:String,
+        name:String,
+        age:Number,
+        designation: String,
+        mobile:Number
+    });
+
+const empdetails=mongoose.model('EmployeeDetails',empdata);
+
+
+app.get('/',(req,res)=>{
+    res.send(htmlfile({}));
+    
+});
+
+app.get('/data',(req,res)=>{
+    empdetails.find().then((data)=>{      
+        res.json(data)});
+    
+});
+
+
+    
+        
+    
+
+
+const hostname="http://localhost:3000/";
+app.post("/",urlencodedparser,(req,res)=>{
+   
+    if(req.body.hidden==1){
+        empdetails.collection.insertOne({id:req.body.id,name: req.body.name,age:req.body.age,designation:req.body.desg,mobile:req.body.mobile })
+        .then(()=>{console.log('inserted successsfully');res.send(`<script>alert('DATA ADDED SUCCESS');window.location.replace('${hostname}');</script>`)})
+        .catch((err)=>{console.error(`connection Problem: ${err}`)});
+    }else if(req.body.hidden==2){
+        empdetails.collection.deleteOne({id:req.body.did})
+        .then(()=>{  res.send(`<script>alert('Deleted');window.location.replace('${hostname}');</script>`)      })
+        .catch((err)=>{console.error(`connection Problem: ${err}`)});
+    }else if(req.body.hidden==3){
+        empdetails.updateOne({id:req.body.id},{id:req.body.idu,name: req.body.nameu,age:req.body.ageu,designation:req.body.desgu,mobile:req.body.mobileu })
+        .then(()=>{  res.send(`<script>alert('Updated');window.location.replace('${hostname}');</script>`)      })
+        .catch((err)=>{console.error(`connection Problem: ${err}`)});
+    }
+  
+});
+   
+
+
+app.listen(port,()=>{console.log(`listening Port localhost:${port}`)});
+    
